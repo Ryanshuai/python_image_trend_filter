a15133415a432961f89bc27718092543cc277859
Ryan <1018718155@qq.com>
2019-10-07 15:43:39 -0700

    update test im

--- generate_test_image.py
+++ generate_test_image.py
@@ -4,19 +4,23 @@ import numpy as np
 
 im_h, im_w = 64, 64
 
-im = np.ones((im_h, im_w))*100
+im = np.ones((im_h, im_w))*130
 
 for i in range(im_h):
     for j in range(im_w):
         if i <= im_h//3 and j >= im_w//3*2:
             im[i][j] = 200
         if i > im_h//3*2 and j <= im_w//3:
-            im[i][j] = 50
-        if i+j <= ((im_w+im_h)/2)//3:
+            im[i][j] = 60
+        if i+j <= ((im_w+im_h)/2)//2:
             im[i][j] = 200
         if i>(im_w+im_h)//4 and j>(im_w+im_h)//4 and \
                 np.sqrt((i-im_h//2)*(i-im_h//2)+(j-im_w//2)*(j-im_w//2)) >= ((im_w+im_h)/4):
-            im[i][j] = 50
+            im[i][j] = 60
+        if i <= im_h//8:
+            im[i][j] = 100
+        if j <= im_w//8:
+            im[i][j] = 160
 
 im = im.astype(np.uint8)
 cv2.imwrite('k0_im.png', im)
--- trend_filter.py
+++ trend_filter.py
@@ -3,7 +3,7 @@ import cvxpy as cp
 import numpy as np
 
 
-class Trend_Fiter:
+class Trend_Fiter_2D:
     """
     n: image w or h
     k: derivative order time
@@ -12,7 +12,6 @@ class Trend_Fiter:
         self.n = n
         self.k = k
         self.make_diff_matrix()
-        self.make_square_matrix()
 
     def make_diff_matrix(self):
         up_right_paradiagonal = np.zeros((self.n, self.n))
@@ -23,44 +22,42 @@ class Trend_Fiter:
         minus_up_right = np.eye(self.n)-down_left_paradiagonal
         minus_down_left = np.eye(self.n)-up_right_paradiagonal
 
-        self.minus_up = minus_up_right[1:self.n-1]
-        self.minus_down = minus_down_left[1:self.n-1]
+        self.minus_up = minus_up_right[1:]
+        self.minus_down = minus_down_left[:self.n-1]
 
-        self.minus_left = minus_down_left[:, 1:self.n-1]
-        self.minus_right = minus_up_right[:, 1:self.n-1]
+        self.minus_left = minus_down_left[:, 1:]
+        self.minus_right = minus_up_right[:, :self.n-1]
 
-    def make_square_matrix(self):
-        self.column_shrink = np.zeros((self.n, self.n-2))
-        for i in range(self.n-2):
-            self.column_shrink[i+1][i] = 1
-
-        self.row_shrink = self.column_shrink.transpose()
+        # print(self.minus_up)
+        # print(self.minus_down)
+        # print(self.minus_left)
+        # print(self.minus_right)
 
     def test_diff(self, mat):
         print('mat_diff_up-----------------------')
-        mat_diff_up = np.matmul(np.matmul(self.minus_up, mat), self.column_shrink)
+        mat_diff_up = np.matmul(self.minus_up, mat)
         print(mat_diff_up)
 
         print('mat_diff_down-----------------------')
-        mat_diff_down = np.matmul(np.matmul(self.minus_down, mat), self.column_shrink)
+        mat_diff_down = np.matmul(self.minus_down, mat)
         print(mat_diff_down)
 
         print('mat_diff_left-----------------------')
-        mat_diff_left = np.matmul(self.row_shrink, np.matmul(mat, self.minus_left))
+        mat_diff_left = np.matmul(mat, self.minus_left)
         print(mat_diff_left)
 
         print('mat_diff_right-----------------------')
-        mat_diff_right = np.matmul(self.row_shrink, np.matmul(mat, self.minus_right))
+        mat_diff_right = np.matmul(mat, self.minus_right)
         print(mat_diff_right)
 
     def solve(self, Y, vlambda=50):
         # Define and solve the CVXPY problem.
         X = cp.Variable((self.n, self.n))
 
-        diff_up = cp.norm(self.minus_up * X * self.column_shrink, 1)
-        diff_down = cp.norm(self.minus_down * X * self.column_shrink, 1)
-        diff_left = cp.norm(self.row_shrink * X * self.minus_left, 1)
-        diff_right = cp.norm(self.row_shrink * X * self.minus_right, 1)
+        diff_up = cp.norm(self.minus_up * X , 1)
+        diff_down = cp.norm(self.minus_down * X , 1)
+        diff_left = cp.norm(X * self.minus_left, 1)
+        diff_right = cp.norm(X * self.minus_right, 1)
 
         obj = 0.5 * cp.sum_squares(Y - X) + vlambda * (diff_up+diff_down+diff_left+diff_right)
 
@@ -78,12 +75,12 @@ if __name__ == '__main__':
     # A2 = np.array([[1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1], [1,1,1,1,1]])
     # A = np.matmul(A1,A2)
 
-    n = 5
+    n = 10
 
     A = np.random.randint(0,20,size=n*n).reshape(n,n)
     print(A)
 
-    tf = Trend_Fiter(5)
+    tf = Trend_Fiter_2D(n)
     # print(tf.column_shrink)
     tf.test_diff(A)
 
